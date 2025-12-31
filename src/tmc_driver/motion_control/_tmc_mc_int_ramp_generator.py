f4b5ae0e6d0530b07619787542cd797c41a2e1bd
Christian Köhlke <christian@koehlke.de>
2025-12-31 12:45:54 +0100

    refactored ramp generator and implemented stop()

--- src/tmc_driver/motion_control/_tmc_mc_int_ramp_generator.py
+++ src/tmc_driver/motion_control/_tmc_mc_int_ramp_generator.py
@@ -11,20 +11,19 @@ import sys
 import time
 from ._tmc_mc import TmcMotionControl, MovementAbsRel, StopMode
 from ..com._tmc_com import TmcCom
-from .._tmc_logger import Loglevel
+from .._tmc_logger import TmcLogger, Loglevel
+from enum import IntEnum
 
 # from .. import _tmc_math as tmc_math
 
 
-# MicroPython compatibility for time functions
-MICROPYTHON = sys.implementation.name == "micropython"
+class RampMode(IntEnum):
+    """Ramp modes of the TMC internal ramp generator"""
 
-
-def _get_time_ms():
-    """Get current time in milliseconds, compatible with both CPython and MicroPython"""
-    if MICROPYTHON:
-        return time.ticks_ms()  # pylint: disable=no-member
-    return time.time_ns() // 1_000_000
+    POSITIONING_MODE = 0
+    VELOCITY_MODE_POS = 1
+    VELOCITY_MODE_NEG = 1
+    HOLD_MODE = 2
 
 
 class TmcMotionControlIntRampGenerator(TmcMotionControl):
@@ -46,6 +45,47 @@ class TmcMotionControlIntRampGenerator(TmcMotionControl):
         self._tmc_com: TmcCom | None = None
         self._starttime: int = 0
 
+    def set_ramp_mode(self, ramp_mode: RampMode):
+        """sets the ramp mode of the internal ramp generator
+
+        Args:
+            ramp_mode (enum): the ramp mode to set
+        """
+        rampmode = self.get_register("rampmode")
+        rampmode.rampmode = int(ramp_mode)
+        rampmode.write()
+
+    def set_motion_profile(self, max_speed: int, acceleration: int, deceleration: int):
+        """sets the motion profile of the internal ramp generator
+        Args:
+            max_speed (int): maximum speed in µsteps/s
+            acceleration (int): acceleration in µsteps/s²
+            deceleration (int): deceleration in µsteps/s²
+        """
+        vstart = self.get_register("vstart")
+        vstart.vstart = 5  # Motor start velocity (unsigned)
+        vstart.write()
+
+        vstop = self.get_register("vstop")
+        vstop.vstop = 10  # Motor stop velocity (unsigned)
+        vstop.write()
+
+        v1 = self.get_register("v1")
+        v1.v1 = 0  # disables A1/D1 phase
+        v1.write()
+
+        vmax = self.get_register("vmax")
+        vmax.vmax = max_speed
+        vmax.write()
+
+        amax = self.get_register("amax")
+        amax.amax = acceleration
+        amax.write()
+
+        dmax = self.get_register("dmax")
+        dmax.dmax = deceleration
+        dmax.write()
+
     def make_a_step(self):
         """method that makes on step"""
         raise NotImplementedError
@@ -57,7 +97,33 @@ class TmcMotionControlIntRampGenerator(TmcMotionControl):
             stop_mode (enum): whether the movement should be stopped immediately or softly
                 (Default value = StopMode.HARDSTOP)
         """
-        super().stop(stop_mode)
+        if stop_mode == StopMode.SOFTSTOP:
+            self.set_ramp_mode(RampMode.VELOCITY_MODE_POS)
+
+            vmax = self.get_register("vmax")
+            vmax.vmax = 0
+            vmax.write()
+
+        if stop_mode == StopMode.HARDSTOP:
+            self.set_ramp_mode(RampMode.VELOCITY_MODE_POS)
+
+            vmax = self.get_register("vmax")
+            vmax.vmax = 0
+            vmax.write()
+
+            amax = self.get_register("amax")
+            amax.amax = 65535  # max deceleration (amax is used in velocity mode for deceleration)
+            amax.write()
+
+    def wait_until_stop(self):
+        """blocks the code until the movement is finished or stopped from a different thread!"""
+        ramp_stat = self.get_register("ramp_stat")
+        while True:
+            ramp_stat.read()
+            if ramp_stat.position_reached:
+                self._tmc_logger.log("position reached", Loglevel.MOVEMENT)
+                return
+            time.sleep(0.01)  # sleep 10ms to reduce CPU load
 
     def run_to_position_steps(
         self, steps, movement_abs_rel: MovementAbsRel | None = None
@@ -77,59 +143,16 @@ class TmcMotionControlIntRampGenerator(TmcMotionControl):
             stop (enum): how the movement was finished
         """
         # pylint: disable=too-many-locals
-        gconf = self.get_register("gconf")
-        gconf.read()
-        gconf.en_pwm_mode = 1
-        gconf.write()
+        if movement_abs_rel is None:
+            movement_abs_rel = self._movement_abs_rel
 
-        xactual = self.get_register("xactual")
-        xactual.read()
-        self._current_pos = xactual.xactual
-        xactual.write()
-
-        rampmode = self.get_register("rampmode")
-        rampmode.rampmode = 0  # Positioning mode
-        rampmode.write()
-
-        vstart = self.get_register("vstart")
-        vstart.vstart = 5
-        vstart.write()
-
-        vstop = self.get_register("vstop")
-        vstop.vstop = 10
-        vstop.write()
-
-        a1 = self.get_register("a1")
-        a1.a1 = self._acceleration
-        a1.write()
-
-        v1 = self.get_register("v1")
-        v1.v1 = self._max_speed
-        v1.write()
-
-        vmax = self.get_register("vmax")
-        vmax.vmax = self._max_speed
-        vmax.write()
-
-        amax = self.get_register("amax")
-        amax.amax = self._acceleration
-        amax.write()
-
-        d1 = self.get_register("d1")
-        d1.d1 = self._acceleration
-        d1.write()
-
-        dmax = self.get_register("dmax")
-        dmax.dmax = self._acceleration
-        dmax.write()
-
-        if movement_abs_rel == MovementAbsRel.ABSOLUTE or (
-            movement_abs_rel is None
-            and self._movement_abs_rel == MovementAbsRel.ABSOLUTE
-        ):
-            self._target_pos = steps
-        else:
+        if movement_abs_rel == MovementAbsRel.RELATIVE:
             self._target_pos = self._current_pos + steps
+        else:
+            self._target_pos = steps
+
+        self.set_ramp_mode(RampMode.POSITIONING_MODE)
+        self.set_motion_profile(self._max_speed, self._acceleration, self._acceleration)
 
         self._tmc_logger.log(
             f"cur: {self._current_pos} | tar: {self._target_pos}", Loglevel.MOVEMENT
@@ -139,10 +162,4 @@ class TmcMotionControlIntRampGenerator(TmcMotionControl):
         xtarget.xtarget = self._target_pos
         xtarget.write()
 
-        ramp_stat = self.get_register("ramp_stat")
-        while True:
-            ramp_stat.read()
-            if ramp_stat.position_reached:
-                self._tmc_logger.log("position reached", Loglevel.MOVEMENT)
-                return self._stop
-            time.sleep(0.01)  # sleep 10ms to reduce CPU load
+        self.wait_until_stop()
