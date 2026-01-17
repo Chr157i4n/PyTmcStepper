95bac7d0449d4b91beb5fcc73c7c8c5fe473c170
Christian Köhlke <christian@koehlke.de>
2026-01-17 22:34:21 +0100

    fix: reorder reset check for consistency

--- src/tmc_driver/reg/_tmc220x_reg.py
+++ src/tmc_driver/reg/_tmc220x_reg.py
@@ -55,12 +55,12 @@ class GStat(shared.GStat):
     def check(self):
         """check if the driver is ok"""
         self.read()
-        if self.reset:
-            raise TmcDriverException("TMC220X: reset detected")
         if self.uv_cp:
             raise TmcDriverException("TMC220X: undervoltage detected")
         if self.drv_err:
             raise TmcDriverException("TMC220X: driver error detected")
+        if self.reset:
+            raise TmcDriverException("TMC220X: reset detected")
 
 
 class IfCnt(shared.IfCnt):
