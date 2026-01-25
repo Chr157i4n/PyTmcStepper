c0c330dc862786cdb93f78ab0cd9722a84eb08a6
Christian Köhlke <christian@koehlke.de>
2026-01-25 12:14:03 +0100

    fix: update import statement to use TmcComSpiMicroPython instead of TmcComUartMicroPython

--- demo/micropython/demo_script_tmc2240_03_basic_movement.py
+++ demo/micropython/demo_script_tmc2240_03_basic_movement.py
@@ -8,7 +8,7 @@ from tmc_driver.tmc_logger import Loglevel
 from tmc_driver.motion_control._tmc_mc import MovementAbsRel
 from tmc_driver.enable_control._tmc_ec_pin import TmcEnableControlPin
 from tmc_driver.motion_control._tmc_mc_step_dir import TmcMotionControlStepDir
-from tmc_driver.com._tmc_com_uart_micropython import TmcComUartMicroPython
+from tmc_driver.com._tmc_com_spi_micropython import TmcComSpiMicroPython
 
 
 print("---")
